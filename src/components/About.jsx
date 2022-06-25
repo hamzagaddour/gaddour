e97293e1e63ba3b3bec902fa4166a31ea033a2fb
GADDOUR <hamza_gaddour@yahoo.com>
2022-06-25 12:48:39 +0100

    init

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-sm">
+          Titulaire d’une Licence Fondamentale en Sciences de l’Informatique, de
+          la Faculté des Sciences de Monastir, et possédant une première
+          expérience en développement WEB, je me permets de vous adresser ma
+          candidature spontanée pour une éventuelle opportunité dans le
+          développement WEB (JS FullStack). Tout au long de ma formation
+          universitaire à la FSM, j’ai maitrisé diverses matières, me permettant
+          en plus de mes stages pratiques, l’acquisition de connaissances et
+          compétences essentielles dans la gestion des projets ainsi que la
+          conception et le développement des applications informatiques
+          répondant à de multiples besoins. Ensuite, j’ai saisi l’opportunité
+          d’effectuer quelques prestations dans le développement des
+          applications WEB. Cette expérience, essentiellement assurée à distance
+          (full remote), m’a permis de découvrir le monde des entreprises et le
+          travail en équipe afin de répondre aux besoins des clients. En
+          parallèle de cette expérience, j’ai également continué à me former par
+          le biais des formations en ligne (Udemy) et en présentiel afin de
+          consolider mes connaissances. Ma toute dernière formation en date
+          (FullStack JS à GOMYCODE, Sousse) s’est déroulée sur 8 mois m’a permis
+          d'enrichir mes connaissances dans le développement WEB et d'acquérir
+          des nouvelles vis à vis des dernières technologies et pratiques de
+          développement.
+        </p>
+
+        <br />
+
+        <p className="text-sm">
+          Je suis de nature ambitieuse et cherchant des expériences
+          professionnelles enrichissantes. J’aime mettre mes compétences à
+          l’épreuve dans un cadre professionnel challengeant et mettant l’accent
+          sur la collaboration et le travail en équipe. Mon dynamisme, le goût
+          du travail en équipe et mon sens de l’écoute, déjà mis en œuvre dans
+          mes précédentes expériences, me faciliteront la contribution et le
+          développement de la mission que vous me confieriez. Je me tiens à
+          votre entière disposition pour tous renseignements complémentaires.
+          Dans l’attente de vous rencontrer, je vous prie d'agréer l'expression
+          de mes respectueuses salutations. 
+          
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Envoyez le formulaire ci-dessous pour me contacter</p>
+        </div>
+
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/92a88fd4-56e7-431b-9c85-d3e4801c110c"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Nom"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
